92713a6611f959749dedd3fa5237053047e1cdef
Henrik Paul <wolfie@Henrik-Pauls-MacBook-Pro-15.local>
2010-01-25 01:28:54 +0200

    build targets should always be annotated @Target

--- build/Default.java
+++ build/Default.java
@@ -1,4 +1,5 @@
 import com.github.wolfie.bob.Build;
+import com.github.wolfie.bob.annotation.Target;
 import com.github.wolfie.bob.result.Action;
 import com.github.wolfie.bob.result.Actions;
 import com.github.wolfie.bob.result.Clear;
@@ -6,7 +7,8 @@ import com.github.wolfie.bob.result.Jar;
 
 public class Default extends Build {
   @Override
+  @Target
   public Action build() {
-    return Actions.from(new Clear(Build.getDefaultBuildDirectory()), new Jar());
+    return Actions.from(new Clear(getDefaultBuildDirectory()), new Jar());
   }
 }
