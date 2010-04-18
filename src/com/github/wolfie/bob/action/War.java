55524c77352b4ab0598219cd9a95f0bf09cc0b90
Henrik Paul <henrik.paul@gmail.com>
2010-04-19 00:11:16 +0300

    completed War API.

--- src/com/github/wolfie/bob/action/War.java
+++ src/com/github/wolfie/bob/action/War.java
@@ -10,7 +10,10 @@ import com.github.wolfie.bob.Util;
 
 public class War extends Jar {
   
+  private static final String WEB_XML_PATH = "WEB-INF/web.xml";
+  
   private String webContentPath = null;
+  private String webXmlPath = null;
   
   @Override
   protected void setDefaults() {
@@ -39,13 +42,29 @@ public class War extends Jar {
   @Override
   protected void subClassProcessHook(final Map<String, File> entryMap) {
     
-    Log.fine("Finding web content files");
+    Log.fine("Finding web content files from " + webContentPath);
     final File webContentDirectory = new File(webContentPath);
     for (final File webFile : getWebContentFiles()) {
+      
       final String entryName = Util.relativeFileName(webContentDirectory,
           webFile);
+      
+      if (webXmlPath != null && entryName.equals(WEB_XML_PATH)) {
+        continue;
+      }
+      
       Log.finer(entryName + " <- " + webFile.getAbsolutePath());
       entryMap.put(entryName, webFile);
+    }
+    
+    if (webXmlPath != null) {
+      final File webXmlFile = new File(webXmlPath);
+      if (webXmlFile.isFile()) {
+        Log.fine("Using " + webXmlFile.getAbsolutePath() + " as web.xml");
+        entryMap.put(WEB_XML_PATH, webXmlFile);
+      } else {
+        Log.warning(webXmlPath + " is not an existing file.");
+      }
     }
     
     super.subClassProcessHook(entryMap);
@@ -54,5 +73,17 @@ public class War extends Jar {
   private Collection<File> getWebContentFiles() {
     Log.fine("Finding web archive files");
     return Util.getFilesRecursively(new File(webContentPath));
+  }
+  
+  public War withWebXmlFrom(final String path) {
+    Util.checkNulls(path);
+    webXmlPath = path;
+    return this;
+  }
+  
+  public War withWebContentFrom(final String path) {
+    Util.checkNulls(path);
+    webContentPath = path;
+    return this;
   }
 }
