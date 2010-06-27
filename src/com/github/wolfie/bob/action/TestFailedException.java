6d92930e1c0710300bce59f8ac3d9db9d6f5d075
Henrik Paul <henrik.paul@gmail.com>
2010-06-28 00:35:40 +0300

    unnecessary exception class

--- src/com/github/wolfie/bob/action/TestFailedException.java
+++
@@ -1,8 +0,0 @@
-package com.github.wolfie.bob.action;
-
-import com.github.wolfie.bob.exception.TestException;
-
-public class TestFailedException extends TestException {
-  private static final long serialVersionUID = 7322091246478528267L;
-  
-}
