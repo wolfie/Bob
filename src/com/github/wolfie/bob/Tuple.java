290cecd4b34a37731534f3cdd255b2e69367773f
Henrik Paul <henrik.paul@gmail.com>
2010-08-16 00:05:04 +0300

    javadoc to tuple methods

--- src/com/github/wolfie/bob/Tuple.java
+++ src/com/github/wolfie/bob/Tuple.java
@@ -10,10 +10,12 @@ public class Tuple<T1, T2> {
     this.t2 = second;
   }
   
+  /** Returns a tuple containing <code>null</code>s */
   public static <T1 extends Object, T2 extends Object> Tuple<T1, T2> ofNull() {
     return of(null, null);
   }
   
+  /** Construct a Tuple out of two objects */
   public static <T1 extends Object, T2 extends Object> Tuple<T1, T2> of(
       final T1 first, final T2 second) {
     return new Tuple<T1, T2>(first, second);
