04167e39d66cfe1f3183defb1d8961a5611da175
Henrik Paul <henrik.paul@gmail.com>
2010-08-22 11:00:36 +0300

    implemented hashCode, equals and toString for Tuple

--- src/com/github/wolfie/bob/Tuple.java
+++ src/com/github/wolfie/bob/Tuple.java
@@ -28,4 +28,48 @@ public class Tuple<T1, T2> {
   public T2 getSecond() {
     return t2;
   }
+  
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((t1 == null) ? 0 : t1.hashCode());
+    result = prime * result + ((t2 == null) ? 0 : t2.hashCode());
+    return result;
+  }
+  
+  @Override
+  public boolean equals(final Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    final Tuple<?, ?> other = (Tuple<?, ?>) obj;
+    if (t1 == null) {
+      if (other.t1 != null) {
+        return false;
+      }
+    } else if (!t1.equals(other.t1)) {
+      return false;
+    }
+    if (t2 == null) {
+      if (other.t2 != null) {
+        return false;
+      }
+    } else if (!t2.equals(other.t2)) {
+      return false;
+    }
+    return true;
+  }
+  
+  @Override
+  public String toString() {
+    return "Tuple [t1=" + t1 + ", t2=" + t2 + "]";
+  }
+  
 }
